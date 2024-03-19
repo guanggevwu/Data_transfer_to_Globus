370c1b3ac8a13df8ac632af6442240b5e09494b3
Qing <guanggevwu@gmail.com>
2024-03-19 11:12:37 -0400

    Added an option to just pass the upper folder. Added readme doc.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Data_transfer_to_Globus
+
 Compress a local directory to zip file and transfer it to a Globus Endpoint. GUI is available.
+
+User instruction:
+
+<ul>
+<li>Directory Path and upload option. "Directory Path" can either be the folder or the folder that contains the subfolders you would like to upload. If it is the former, select "upload the entered folder". If it is the latter, select "upload the subfolders in the entered folder".</li>
+<li>Connect to Globus. Click "Connect to Globus", a broswer window and a new program dialog window will open. In the broswer window, log in if it is required. Then copy the code, paste into the new program dialog window and click "Authorize". A common error here would be that someone else (who doesn't has the permission) has logged in Globus in the broswer. In this case, you will need to log out and log in your Globus account.</li>
+<li>Compression and Upload. If "upload the entered folder" is selected, then the entered folder will be compressed and upload, even if the folder has been compressed or uploaded before. If "upload the subfolders in the entered folder" is selected, then only sub folders that haven't be compressed before will be compressed. Also if the a compressed file is detected in the destination endpoint, the upload request will be ignored.</li>
+</ul>
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter import ttk
 import webbrowser
 from threading import Thread
 import time
+from datetime import datetime
 
 
 class DataUpload:
@@ -14,7 +15,7 @@ class DataUpload:
         self.count = 0
         with open("start_up.txt") as file:
             self.lines = file.readlines()
-            self.start_up_path, self.CLIENT_ID, self.start_up_source_endpoint, self.start_up_destination_endpoint = [
+            self.start_up_path, self.CLIENT_ID, self.start_up_source_endpoint, self.start_up_destination_endpoint, self.start_up_folder_option = [
                 line.strip() for line in self.lines]
         root.title("Upload data to UM Data Den")
         s = ttk.Style()
@@ -26,26 +27,36 @@ class DataUpload:
             root, text='Data Information', padding=pad_widget, style='Sty1.TLabelframe')
         frame1.grid(column=0, row=0, sticky=(N, W, E, S))
 
-        ttk.Label(frame1, text='Directory Path: ').grid(
+        ttk.Label(frame1, text='Souece Directory: ').grid(
             column=1, row=1, columnspan=1, sticky='W')
         self.dir_var = StringVar(value=self.start_up_path)
+        self.input_path = self.start_up_path
         dir_entry = ttk.Entry(frame1, width=80, textvariable=self.dir_var)
+        dir_entry.bind("<FocusOut>", lambda x: self.save_dir_entry())
         dir_entry.grid(column=2, row=1, columnspan=3, sticky=(W))
 
+        self.folder_option = StringVar(value=self.start_up_folder_option)
+        upload_current = ttk.Radiobutton(
+            frame1, text='upload the entered folder', variable=self.folder_option, value='upload_current', command=self.upload_current_or_sub)
+        upload_current.grid(column=1, row=2, columnspan=2, sticky=(W, E))
+        upload_sub = ttk.Radiobutton(
+            frame1, text='upload the subfolders in the entered folder', variable=self.folder_option, value='upload_sub', command=self.upload_current_or_sub)
+        upload_sub.grid(column=3, row=2, columnspan=2, sticky=(W, E))
+
         ttk.Label(frame1, text='Source Endpoint: ').grid(
-            column=1, row=2, columnspan=1, sticky='')
+            column=1, row=3, columnspan=1, sticky='W')
         self.source_var = StringVar(value=self.start_up_source_endpoint)
         source_entry = ttk.Entry(
             frame1, width=20, textvariable=self.source_var)
-        source_entry.grid(column=2, row=2, columnspan=1, sticky=(W))
+        source_entry.grid(column=2, row=3, columnspan=1, sticky=(W))
 
         ttk.Label(frame1, text='Destination Endpoint: ').grid(
-            column=3, row=2, columnspan=1, sticky='E')
+            column=3, row=3, columnspan=1, sticky='W')
         self.destination_var = StringVar(
             value=self.start_up_destination_endpoint)
         destinstion_entry = ttk.Entry(
             frame1, width=20, textvariable=self.destination_var, state="disabled")
-        destinstion_entry.grid(column=4, row=2, columnspan=1, sticky=(W))
+        destinstion_entry.grid(column=4, row=3, columnspan=1, sticky=(W))
 
         frame2 = ttk.Labelframe(root, text='Operation',
                                 padding=pad_widget, style='Sty1.TLabelframe')
@@ -60,14 +71,14 @@ class DataUpload:
                              command=self.compression_and_upload)
         self.b2.grid(column=1, row=0, columnspan=1, sticky=[W, E])
 
-        self.frame3 = ttk.Labelframe(
+        frame3 = ttk.Labelframe(
             root, text='Task Information', padding=pad_widget, style='Sty1.TLabelframe')
-        self.frame3.grid(column=0, row=2, sticky=(N, W, E, S))
+        frame3.grid(column=0, row=2, sticky=(N, W, E, S))
 
-        self.t = Text(self.frame3, width=70, height=10, wrap=WORD)
+        self.t = Text(frame3, width=70, height=10, wrap=WORD)
         self.t.grid(column=0, row=0, columnspan=1, sticky=W)
         self.t.insert(INSERT, "Started.\n")
-        sb = ttk.Scrollbar(self.frame3,
+        sb = ttk.Scrollbar(frame3,
                            orient='vertical',
                            command=self.t.yview)
 
@@ -76,21 +87,48 @@ class DataUpload:
         self.t['state'] = 'disabled'
 
         self.status_label = ttk.Label(
-            self.frame3, text="", foreground="red", cursor="hand2", font=('Times', 12, 'underline'))
+            frame3, text="", foreground="red", cursor="hand2", font=('Times', 12, 'underline'))
 
-        for child in [frame1.winfo_children()[childidx] for childidx in [0, 1]]:
-            child.grid_configure(padx=[0, 0], pady=3)
+        for child in frame1.winfo_children():
+            child.grid_configure(padx=[0, 0], pady=5)
 
         for child in [frame2.winfo_children()[childidx] for childidx in [0, 1]]:
             child.grid_configure(padx=[0, 0], pady=3)
 
-        for child in [self.frame3.winfo_children()[childidx] for childidx in [0]]:
+        for child in [frame3.winfo_children()[childidx] for childidx in [0]]:
             child.grid_configure(padx=[0, 0], pady=3)
 
+        self.upload_current_or_sub()
+
+    def save_dir_entry(self):
+        with open("start_up.txt", 'w') as file:
+            self.lines[0] = self.dir_var.get() + "\n"
+            file.writelines(self.lines)
+        self.input_path = self.dir_var.get()
+
+    def upload_current_or_sub(self):
+        if self.folder_option.get() == "upload_current":
+            self.upload_folders_full = [self.input_path]
+            self.upload_folders = [self.input_path.split(os.sep)[-1]]
+            self.upper_folder = os.path.dirname(self.input_path)
+        elif self.folder_option.get() == "upload_sub":
+            self.upload_folders_full = [os.path.join(self.input_path, e) for e in os.listdir(
+                self.input_path) if os.path.isdir(os.path.join(self.input_path, e))]
+            self.upload_folders = [e.split(os.sep)[-1]
+                                   for e in self.upload_folders_full]
+            self.upper_folder = self.input_path
+        self.folder_option_value = self.folder_option.get()
+        with open("start_up.txt", 'w') as file:
+            self.lines[4] = self.folder_option_value + "\n"
+            file.writelines(self.lines)
+
+    def get_time_string(self):
+        return datetime.now().strftime('%m/%d %H:%m:%S')
+
     def insert_to_disabled(self, t, text):
-        self.t['state'] = 'normal'
-        self.t.insert(END, text)
-        self.t['state'] = 'disabled'
+        t['state'] = 'normal'
+        t.insert(END, f'{self.get_time_string()}, {text}')
+        t['state'] = 'disabled'
 
     def connect(self):
         with open("start_up.txt", 'w') as file:
@@ -138,81 +176,113 @@ class DataUpload:
         self.ZEUS_app.gm = globus_sdk.GroupsManager(self.ZEUS_app.gc)
 
         # use first search result
+        if not list(self.ZEUS_app.tc.endpoint_search(self.source_var.get())):
+            self.insert_to_disabled(
+                self.t, f"The logged in Globus account doesn't has the permission to the source endpoint.\n")
+            return
         self.ZEUS_app.source_endpoint_id = list(
             self.ZEUS_app.tc.endpoint_search(self.source_var.get()))[0]["id"]
-        if not self.ZEUS_app.source_endpoint_id:
-            self.insert_to_disabled(self.t, f"Can't reach source endpoint.\n")
-            return
-        self.ZEUS_app.destination_endpoint_id = list(
-            self.ZEUS_app.tc.endpoint_search(self.destination_var.get()))[0]["id"]
-        if not self.ZEUS_app.source_endpoint_id:
+
+        if not list(self.ZEUS_app.tc.endpoint_search(self.destination_var.get())):
             self.insert_to_disabled(
                 self.t, f"Can't reach destination endpoint.\n")
             return
+        self.ZEUS_app.destination_endpoint_id = list(
+            self.ZEUS_app.tc.endpoint_search(self.destination_var.get()))[0]["id"]
+
         self.insert_to_disabled(self.t, "Connected to Globus successfully!\n")
         self.connected = True
 
     def compression_and_upload(self):
         if not self.connected:
-            self.insert_to_disabled(self.t, "Please connect to Globus first!\n")
+            self.insert_to_disabled(
+                self.t, "Please connect to Globus first!\n")
             return
-        with open("start_up.txt", 'w') as file:
-            self.lines[0] = self.dir_var.get() + "\n"
-            file.writelines(self.lines)
-        self.directory_path = self.dir_var.get()
-        if not os.path.exists(self.directory_path):
+        if not os.path.exists(self.input_path):
             self.insert_to_disabled(
                 self.t, "Invalid directory path or file path. Please check again.\n")
             return
-        self.dir_parts = self.directory_path.split(os.sep)
+        if self.upload_folders_full and not self.upload_folders_full[0].split(os.sep)[-3].isdigit():
+            self.insert_to_disabled(
+                self.t, "Wrong combination of directory name and upload option!\n")
+            return
 
         def threaded_function(arg):
             shutil.make_archive(arg, 'zip', arg)
-        thread = Thread(target=threaded_function, args=(self.directory_path, ))
+        threads = []
+        self.compressed_folders_short = []
+        for full, short in zip(self.upload_folders_full, self.upload_folders):
+            if self.folder_option_value == "upload_sub" and os.path.exists(full+'.zip'):
+                continue
+            threads.append(Thread(target=threaded_function, args=(full, )))
+            self.compressed_folders_short.append(short)
+        for t in threads:
+            t.start()
+        self.monitor(threads)
 
-        thread.start()
-        self.monitor(thread)
+    def any_thread_alive(self, threads):
+        for t in threads:
+            if t.is_alive():
+                return True
+        else:
+            return False
 
-    def monitor(self, thread):
+    def monitor(self, threads):
         if self.count == 0:
-            self.insert_to_disabled(self.t, f"Compressing.\n")
+            self.insert_to_disabled(
+                self.t, f"Compressing {', '.join(self.compressed_folders_short)}.\n")
+
         self.count += 1
-        if thread.is_alive():
+        if self.any_thread_alive(threads):
             # check the thread every 100ms
             self.t['state'] = 'normal'
             self.t.delete("end -2 lines", "end -1 lines")
-            self.t.insert(
-                "end", f"Compressing...Elapsed time: {self.count} s.\n")
             self.t['state'] = 'disabled'
+            self.insert_to_disabled(
+                self.t, f"Compressing {', '.join(self.compressed_folders_short)}...Elapsed time: {self.count} s.\n")
             self.t.see("end")
-            root.after(1000, lambda: self.monitor(thread))
+            root.after(1000, lambda: self.monitor(threads))
         else:
-
-            self.insert_to_disabled(self.t, "Compression finished.\n")
+            if self.compressed_folders_short:
+                self.insert_to_disabled(
+                    self.t, f"Compression is finished. Target folder: {', '.join(self.compressed_folders_short)}\n")
+            else:
+                self.insert_to_disabled(
+                    self.t, "Warning! Compression is terminated. All the files were compressed before.\n")
             self.count = 0
 
             # create a Transfer task consisting of one or more items
             task_data = globus_sdk.TransferData(
                 source_endpoint=self.ZEUS_app.source_endpoint_id, destination_endpoint=self.ZEUS_app.destination_endpoint_id
             )
-            task_data.add_item(
-                "/"+(self.directory_path + ".zip").replace(":",
-                                                           "").replace("\\", "/"),  # source
-                os.path.join(
-                    "/experimental_data", self.dir_parts[-3], self.dir_parts[-2],  self.dir_parts[-1]).replace("\\", "/")+".zip",  # dest
 
-            )
+            folders_to_transfer = []
+            for fullname, name in zip(self.upload_folders_full, self.upload_folders):
+                # if there is  a file with the same name in the destination folder, skip. Else, add item.
+                if f'{name}.zip' not in [e['name'] for e in self.ZEUS_app.tc.operation_ls(self.ZEUS_app.destination_endpoint_id, path=os.path.join(
+                        "/experimental_data", *self.upper_folder.split(os.sep)[-2:]).replace("\\", "/"))] or self.folder_option_value == "upload_current":
+                    task_data.add_item(
+                        "/"+(fullname + ".zip").replace(":",
+                                                        "").replace("\\", "/"),  # source
+                        os.path.join(
+                            "/experimental_data", *self.upper_folder.split(os.sep)[-2:], name).replace("\\", "/")+".zip",  # dest
+                    )
+                    folders_to_transfer.append(f'{name}.zip')
 
-            # submit, getting back the task ID
-            task_doc = self.ZEUS_app.tc.submit_transfer(task_data)
-            task_id = task_doc["task_id"]
-            self.insert_to_disabled(
-                self.t, f"Submitted transfer. Task_id: {task_id}.\n")
+            if not folders_to_transfer:
+                self.insert_to_disabled(
+                    self.t, f"Warning! Task is terminated. All the files already exist in the destination.\n")
+            else:
+                # submit, getting back the task ID
+                task_doc = self.ZEUS_app.tc.submit_transfer(task_data)
+                task_id = task_doc["task_id"]
+                self.insert_to_disabled(
+                    self.t, f"Transfer is submitted. Task_id: {task_id}. Target files: {', '.join(folders_to_transfer)}\n")
 
-            self.status_label.config(text="Check transfer progress")
-            self.status_label.grid(column=0, row=1, columnspan=1, sticky=W)
-            self.status_label.bind(
-                "<Button-1>", lambda e: webbrowser.open_new("https://app.globus.org/activity"))
+                self.status_label.config(text="Check transfer progress")
+                self.status_label.grid(column=0, row=1, columnspan=1, sticky=W)
+                self.status_label.bind(
+                    "<Button-1>", lambda e: webbrowser.open_new("https://app.globus.org/activity"))
 
 
 class NewWindow(Toplevel):
